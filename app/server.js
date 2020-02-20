2ae77d85f4b40c52c692ef79e72003be696ca0d5
alexanto222 <37571008+alexanto222@users.noreply.github.com>
2020-02-20 19:35:45 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -7,12 +7,8 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('*** azure devops-aks-5.0 ***\n');
+  res.send('*** AZure Devops - AKS -7.0 ***\n');
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
-
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
